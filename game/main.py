f8d8bd9f1dc79d80206b2b7a6e71ee62f0e09106
alexandrebarbaruiva <lexander.santos@gmail.com>
2018-07-06 00:32:28 -0300

    Improve start_game()

--- game/main.py
+++ game/main.py
@@ -1,6 +1,19 @@
 def start_game():
     print("Welcome to DevSim")
-    return True
+    print("1. Play")
+    print("2. Load")
+    print("3. Quit")
+    try:
+        choice = int(input("Your choice: "))
+    except Exception:
+        choice = 0
+    while (choice < 1 or choice > 3):
+        print("Choice not valid.")
+        try:
+            choice = int(input("Your choice: "))
+        except Exception:
+            choice = 0
+    return choice
 
 
 if __name__ == '__main__':
--- tests/unit_tests/test_main.py
+++ tests/unit_tests/test_main.py
@@ -1,8 +1,34 @@
 import unittest
+from unittest.mock import patch
 from game.main import start_game
 
 
 class TestGameMenu(unittest.TestCase):
+
     def test_start_game(self):
-        """Check if game starts properly"""
-        self.assertEqual(start_game(), True, msg="Missing menu")
+        """Check if game starts properly."""
+        expected = 1
+        with patch("builtins.input", return_value=expected):
+            self.assertEqual(start_game(), expected,
+                             msg="Missing Start Game option")
+
+    def test_start_game_with_wrong_input_type(self):
+        """Check if game doesn't start when passed wrong input."""
+        expected = ("a", 1)
+        with patch("builtins.input", side_effect=expected):
+            self.assertEqual(start_game(), 1,
+                             msg="Missing Start Game option")
+
+    def test_start_game_with_multiple_wrong_input_types(self):
+        """Check if game doesn't start when passed multiple wrong inputs."""
+        expected = ("a", "b", "c", 2)
+        with patch("builtins.input", side_effect=expected):
+            self.assertEqual(start_game(), 2,
+                             msg="Missing Start Game option")
+
+    def test_start_game_with_wrong_input_number(self):
+        """Check if game doesn't start when passed wrong input number."""
+        expected = (5, 1)
+        with patch("builtins.input", side_effect=expected):
+            self.assertEqual(start_game(), 1,
+                             msg="Missing Start Game option")
