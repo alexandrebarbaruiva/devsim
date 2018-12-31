19003a142079c5422a25d3271d9675c9d7190be4
alexandrebarbaruiva <alexandrebarbaruiva@users.noreply.github.com>
2018-12-31 16:28:34 -0200

    Improve game

--- game/main.py
+++ game/main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from random import random
 from time import sleep
 sys.path.append(
     os.path.dirname(
@@ -7,6 +8,7 @@ sys.path.append(
     )
 )
 from game.company import Company
+from game.software import Software
 
 
 def start_game():
@@ -22,7 +24,7 @@ def choose(init=1, end=3):
         choice = int(input("Your choice: "))
     except Exception:
         choice = 0
-    while (choice < init or choice > end):
+    while choice < init or choice > end:
         print("Choice not valid.")
         try:
             choice = int(input("Your choice: "))
@@ -31,20 +33,46 @@ def choose(init=1, end=3):
     return choice
 
 
-def action_turn():
-    return choose()
+def run_turn(player_company):
+    """
+
+    :type player_company: Company
+    """
+    game = True
+    turn = 0
+    while game:
+        turn = display_turn(turn, player_company)
+        action_turn(player_company)
+        if turn >= 4:
+            game = decide_players_fate()
+
+
+def action_turn(player_company):
+    print(f"1. Create software\n"
+          f"2. Program\n"
+          f"3. Hire new employee")
+    chosen = choose()
+    if chosen == 1:
+        soft_name = input("What is the software's name? ")
+        player_company.stats["software"].append(Software(name=soft_name))
+        print("Software starting development now.")
+    elif chosen == 2:
+        pass
+    else:
+        pass
+    return 1
 
 
 def display_turn(turn=0, company=None):
     os.system('clear')
     if company:
-        print("-----------------------------")
-        print(f"Company: {company.stats['name']}")
-        print(f"{company.stats['age']} year(s) old, "
+        print("-----------------------------\n"
+              f"Company: {company.stats['name']}\n"
+              f"{company.stats['age']} year(s) old, "
               f"{company.stats['fans']} fan(s), "
-              f"{company.stats['rating']} star(s)."
-        )
-        print(f"Turn: {turn}")
+              f"{company.stats['rating']} star(s).\n"
+              f"Products released: {len(company.stats['software'])}\n"
+              f"Turn: {turn}")
         print("-----------------------------")
         sleep(1)
     return turn + 1
@@ -66,9 +94,32 @@ def main():
     if a == 1:
         player_company = Company(set_up_player())
         print(player_company.stats)
-        turn = 0
-        while turn < 3:
-            turn = display_turn(turn, player_company)
+        run_turn(player_company)
+    if a == 3:
+        return 0
+    return 0
+
+
+def decide_players_fate():
+    if random() > 0.3:
+        win_game()
+    lose_game()
+    return False
+
+
+def win_game():
+    print(
+        "You have dominated the software industry.\n"
+        "Congratulations!!! The game is finally over!"
+    )
+    return 0
+
+
+def lose_game():
+    print(
+        "You have been bested by your fellow colleagues.\n"
+        "Good luck next time."
+    )
     return 0
 
 
--- tests/unit_tests/test_main.py
+++ tests/unit_tests/test_main.py
@@ -43,39 +43,40 @@ class TestGameMenu(unittest.TestCase):
                 set_up_player(), "Rich Company",
                 msg="Game did not properly run")
 
-    def test_choose_action(self):
-        with patch("builtins.input", return_value=1):
-            self.assertEqual(
-                action_turn(), 1,
-                msg="Game did not properly return expected choice")
-
-    def test_choose_action_wrong_input(self):
-        expected = ("a", 2)
-        with patch("builtins.input", side_effect=expected):
-            self.assertEqual(
-                action_turn(), 2,
-                msg="Game did not properly return expected choice")
-
 
 class TestTurns(unittest.TestCase):
     """Test what each game turn returns."""
-    def test_turn_choice_behavior(self):
-        with patch("builtins.input", return_value=1):
-            self.assertEqual(
-                action_turn(), 1,
-                msg="Game did not properly return expected action")
+    @patch('game.company.Company')
+    def test_choose_action(self, company):
+        company.stats = {"name": "Test", "software": []}
+        expected = (1, "name")
+        with patch.dict(company.stats):
+            with patch("builtins.input", side_effect=expected):
+                self.assertEqual(
+                    action_turn(company), 1,
+                    msg="Game did not properly return expected choice")
 
+    @patch('game.company.Company')
+    def test_choose_action_wrong_input(self, company):
+        expected = ("a", 1, "name")
+        company.stats = {"name": "Test", "software": []}
+        with patch.dict(company.stats):
+            with patch("builtins.input", side_effect=expected):
+                self.assertEqual(
+                    action_turn(company), 1,
+                    msg="Game did not properly return expected choice")
 
     def test_turn_behavior(self):
-        self.assertEqual(display_turn(), 1, msg="Turn not registered successfuly")
+        self.assertEqual(
+            display_turn(), 1, msg="Turn not registered successfully")
 
     @patch('game.company.Company')
     def test_turn_behavior_with_company(self, company):
-        company.stats = {"name": "Test", "age": 0, "fans": 0, "rating": 0}
+        company.stats = {"name": "Test", "age": 0, "fans": 0, "rating": 0, "software": []}
         with patch.dict(company.stats):
             self.assertEqual(
                 display_turn(company=company), 1,
-                msg="Turn not registered successfuly"
+                msg="Turn not registered successfully"
             )
 
 
